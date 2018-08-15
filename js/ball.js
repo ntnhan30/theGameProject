194601ad9c8f6d232dc60affdea3a8aa061741ff
trong nhan nguyen <ntnhan30@gmail.com>
2018-08-15 15:46:35 +0200

    backgroundChange

--- js/ball.js
+++ js/ball.js
@@ -23,7 +23,7 @@ function Ball(x, y, vx, vy, radius) {
   Ball.prototype.draw = function () {
     ctx.save();
     ctx.font = "30px Jua"
-    ctx.fillText(" Life " , this.x, this.y)
+    ctx.fillText(" Life " , this.x, this.y-20)
     ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
